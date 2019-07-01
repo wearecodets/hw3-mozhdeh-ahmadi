29c3f6025d95f3004e837ee1e2e14044daa66adc
Niko Solihin <nikosolihin@gmail.com>
2019-07-01 15:05:56 +0700

    implement accordion

--- src/main.js
+++ src/main.js
@@ -1,1 +1,17 @@
-console.log('Hello, world!');
+const accordions = document.querySelectorAll('.accordion');
+const activeClass = 'accordion--active';
+
+function expand(item) {
+  if (item.classList.contains(activeClass)) {
+    item.classList.remove(activeClass);
+  } else {
+    accordions.forEach(i => i.classList.remove(activeClass));
+    item.classList.add(activeClass);
+  }
+}
+
+accordions.forEach(function(item) {
+  item.addEventListener('click', function() {
+    expand(item);
+  });
+});
