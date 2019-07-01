df915811e0e05e90e295dfc7782f8e8c5aa7b150
Niko Solihin <nikosolihin@gmail.com>
2019-07-01 23:03:40 +0700

    answer key

--- src/main.js
+++ src/main.js
@@ -1,1 +1,25 @@
-console.log('Hello, world!');
+const form = document.querySelector('.form');
+const total = document.querySelector('.form__total');
+const payButton = document.querySelector('.form__pay');
+const payButtonTotal = payButton.querySelector('span');
+const paypalLink = document.querySelector('a.form__cc-heading');
+let amount = 0;
+
+function calculateTotal() {
+  const frequency = form.plan.value.toLowerCase();
+  amount = frequency === 'monthly' ? 30 : 25 * 12;
+}
+
+function updateTotal() {
+  payButton.disabled = false;
+  total.innerText = `$${amount}`;
+  payButtonTotal.innerText = `$${amount}`;
+  paypalLink.setAttribute('href', `https://paypal.me/nikosolihin/${amount}`);
+}
+
+form.plan.forEach(function(plan) {
+  plan.addEventListener('change', function() {
+    calculateTotal();
+    updateTotal();
+  });
+});
