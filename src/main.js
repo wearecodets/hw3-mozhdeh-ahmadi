c1e6262abc5b4a60e030ee94a5c50d16f781f949
Niko Solihin <nikosolihin@gmail.com>
2019-07-03 11:11:43 +0700

    add comments

--- src/main.js
+++ src/main.js
@@ -1,17 +1,18 @@
 const accordions = document.querySelectorAll('.accordion');
 const activeClass = 'accordion--active';
 
-function expand(item) {
-  if (item.classList.contains(activeClass)) {
-    item.classList.remove(activeClass);
-  } else {
-    accordions.forEach(i => i.classList.remove(activeClass));
-    item.classList.add(activeClass);
-  }
-}
-
 accordions.forEach(function(item) {
   item.addEventListener('click', function() {
-    expand(item);
+    if (item.classList.contains(activeClass)) {
+      // I'm currently active, so just close myself.
+      item.classList.remove(activeClass);
+    } else {
+      // I'm not active, so close the other first.
+      accordions.forEach(function(others) {
+        others.classList.remove(activeClass);
+      });
+      // Then activate myself.
+      item.classList.add(activeClass);
+    }
   });
 });
