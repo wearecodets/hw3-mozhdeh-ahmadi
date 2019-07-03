4adda85969ecff485f326610d879c321ca0f25da
Niko Solihin <nikosolihin@gmail.com>
2019-07-03 09:42:48 +0700

    use more vars

--- src/main.js
+++ src/main.js
@@ -1,19 +1,22 @@
+const monthlyCost = 30;
+const annualCost = 25;
+let amount = 0;
+
 const form = document.querySelector('.form');
 const total = document.querySelector('.form__total');
 const payButton = document.querySelector('.form__pay');
 const payButtonTotal = payButton.querySelector('span');
 const paypalLink = document.querySelector('a.form__cc-heading');
-let amount = 0;
 
 function calculateTotal() {
   const frequency = form.plan.value.toLowerCase();
-  amount = frequency === 'monthly' ? 30 : 25 * 12;
+  amount = frequency === 'monthly' ? monthlyCost : annualCost * 12;
 }
 
 function updateTotal() {
   payButton.disabled = false;
-  total.innerText = `$${amount}`;
-  payButtonTotal.innerText = `$${amount}`;
+  total.textContent = `$${amount}`;
+  payButtonTotal.textContent = `$${amount}`;
   paypalLink.setAttribute('href', `https://paypal.me/nikosolihin/${amount}`);
 }
 
